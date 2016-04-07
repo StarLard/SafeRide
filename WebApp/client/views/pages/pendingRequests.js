7181cf5815a6b72ea2300075bdb99033af8afb48
Dylan Secreast <dsecreast@gmail.com>
2016-04-06 18:03:48 -0700

    Removed pending queue

--- WebApp/client/views/pages/pendingRequests.js
+++ WebApp/client/views/pages/pendingRequests.js
@@ -7,24 +7,11 @@ if (Meteor.isClient) {
       return Pending.find({});
     }
   });
-
 }
 
 // Execute server-side
 if (Meteor.isServer) {
   Meteor.startup(function() {
-
-    // Seed db if empty
-    if (Pending.find().count() == 0) {
-      Pending.insert({
-        name: "John Doe",
-        phone: "555-555-5555",
-        uoid: 951111111,
-        pickup: "[Pickup Address]",
-        dropoff: "[Dropoff Address]",
-        riders: 3
-      });
-    }
 
   });
 }
