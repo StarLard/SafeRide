db56b3e882c890597e938c90b966d5820e11aac4
Zachary Jones <Zauran@liadrin.local>
2016-04-06 19:41:33 -0700

    Added basic Settings screen

---
+++ SafeRide/SafeRide/SettingsCell.swift
@@ -0,0 +1,15 @@
+//
+//  SettingsCell.swift
+//  SafeRide
+//
+//  Created by Zachary Jones on 4/6/16.
+//  Copyright © 2016 University of Oregon. All rights reserved.
+//
+
+import UIKit
+
+class SettingsCell: UITableViewCell {
+    // MARK: Properties (IBOutlet)
+    @IBOutlet weak var settingsLabel: UILabel!
+    @IBOutlet weak var settingsField: UITextField!
+}
