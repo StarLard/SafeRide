d6a57d6f90d6403951b23a462e6d839e851ea145
Dylan Secreast <dsecreast@gmail.com>
2016-04-09 01:32:08 -0700

    Added fixed-nav

--- WebApp/client/views/common/top-navbar.js
+++ WebApp/client/views/common/top-navbar.js
@@ -2,8 +2,8 @@ Template.topNavbar.rendered = function(){
 
     // FIXED TOP NAVBAR OPTION
     // Uncomment this if you want to have fixed top navbar
-    // $('body').addClass('fixed-nav');
-    // $(".navbar-static-top").removeClass('navbar-static-top').addClass('navbar-fixed-top');
+    $('body').addClass('fixed-nav');
+    $(".navbar-static-top").removeClass('navbar-static-top').addClass('navbar-fixed-top');
 
 };
 
