e10eb69cc141e12bdd00a2817f0712ed45317ad1
Dylan Secreast <dsecreast@gmail.com>
2016-04-12 14:13:26 -0700

    Added variables to grab all info from POST

--- WebApp/client/views/pages/test.php
+++ WebApp/client/views/pages/test.php
@@ -1,21 +1,26 @@
 <?php
-    echo " Response From Server Side";
-    echo "Username : " . $_REQUEST['username'];
-    echo "Password : " . $_REQUEST['password'];
+    $name = $_REQUEST['name'];
+    $uoid = $_REQUEST['uoid'];
+    $phone = $_REQUEST['phone'];
+    $pickup = $_REQUEST['pickup'];
+    $dropoff = $_REQUEST['dropoff'];
+    $riders = $_REQUEST['riders'];
+    $pickupTime = $_REQUEST['pickuptime'];
 
     $connection = new MongoClient("mongodb://<USERNAME>:<PASSWORD>@ds061375.mlab.com:61375/saferide");
     $db = $m->meteor;
     $collection = $db->pending;
 
     $document = array(
-      "name" => "<NAME>",
-      "uoid" => "<UOID>",
-      "phone" => "<PHONE>",
-      "pickup" => "<PICKUP>",
-      "dropoff" => "<DROPOFF>",
-      "riders" => "<RIDERS>",
-      "pickupTime" => "<PICKUPTIME>"
+      "name" => $name,
+      "uoid" => $uoid,
+      "phone" => $phone,
+      "pickup" => $pickup,
+      "dropoff" => $dropoff,
+      "riders" => $riders,
+      "pickupTime" => $pickupTime
     );
 
     $collection->insert($document);
+    echo "Successfully added iOS pending request";
 ?>
