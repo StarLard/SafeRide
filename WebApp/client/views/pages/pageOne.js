d1bbb1153a51c8882fe4d5204474dbfcc1fcc7c6
Dylan Secreast <dsecreast@gmail.com>
2016-04-19 14:45:26 -0700

    Cleaned up code

--- WebApp/client/views/pages/pageOne.js
+++ WebApp/client/views/pages/pageOne.js
@@ -10,25 +10,6 @@ if (Meteor.isClient) {
         result = "There is currently 1 pending request";
       } else if (count > 1) {
         result = "There are currently " + count + " pending requests";
-
-        // toastr.options = {
-        //   "closeButton": false,
-        //   "debug": false,
-        //   "progressBar": false,
-        //   "preventDuplicates": true,
-        //   "positionClass": "toast-bottom-full-width",
-        //   "onclick": null,
-        //   "showDuration": "0",
-        //   "hideDuration": "0",
-        //   "timeOut": "0",
-        //   "extendedTimeOut": "0",
-        //   "showEasing": "swing",
-        //   "hideEasing": "linear",
-        //   "showMethod": "fadeIn",
-        //   "hideMethod": "fadeOut"
-        // }
-        // toastr.warning("There are " + count + " pending requests that need your attention!");
-
       }
       return result;
     },
