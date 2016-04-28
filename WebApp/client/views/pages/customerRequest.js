bbc1ddb18a4f8a1b072b5675d081e29766e6c736
Dylan Secreast <dsecreast@gmail.com>
2016-04-27 18:25:26 -0700

    Added insert function

--- WebApp/client/views/pages/customerRequest.js
+++ WebApp/client/views/pages/customerRequest.js
@@ -1,1 +1,22 @@
-// Router.route('/customerRequest');
+Template.customerRequest.rendered = function(){
+    // Move modal to body
+    // Fix Bootstrap backdrop issue with animation.css
+    $('.modal').appendTo("body");
+};
+
+storeRequest = function(name, uoid, phone, pickup, dropoff, riders, pickupTime) {
+  Pending.insert({
+    name: name,
+    phone: phone,
+    uoid: uoid,
+    pickup: pickup,
+    dropoff: dropoff,
+    riders: riders,
+    pickupTime: pickupTime,
+    createdTime: moment().format('h:mm a')
+  });
+}
+
+validateForm = function() {
+  
+}
