bb871fd68ae401f1f4390ab606f1c912172610cf
Dylan Secreast <dsecreast@gmail.com>
2016-05-18 03:21:10 -0700

    Added logout functionality

--- WebApp/client/views/common/navigation.js
+++ WebApp/client/views/common/navigation.js
@@ -1,6 +1,11 @@
 Template.navigation.rendered = function(){
-
     // Initialize metisMenu
     $('#side-menu').metisMenu();
-
 };
+
+Template.navigation.events({
+    'click .logout': function(e){
+        e.preventDefault();
+        Meteor.logout();
+    }
+});
